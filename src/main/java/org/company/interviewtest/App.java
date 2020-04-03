5a914c38fb92c47b359ff62acfd8a4b6c3197dc6
lgoda <luca.godano@gmail.com>
2020-04-03 10:38:34 +0200

    Update App.java

--- src/main/java/org/company/interviewtest/App.java
+++ src/main/java/org/company/interviewtest/App.java
@@ -6,7 +6,7 @@ public class App {
 
     public static void main(String[] args) {
         FileCopierFacade fileCopier = new FileCopierFacade();
-        // "C://Users/ft20fd/development/gc.log" C://Users/ft20fd/development/gc-tmp.log
+        
         try {
             fileCopier.copyFile(args[0], args[1]);
         } catch (IOException | InterruptedException e) {
